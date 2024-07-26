03e8191e491288248b8f53a90a57335343b8a2b6
Ivan Nieto Vivanco <inietov@gmail.com>
2024-07-26 14:20:45 -0600

    Add languages endpoint

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ async def get_regions(request: Request):
     cursor = connection.cursor()
 
     cursor.execute("CREATE TABLE IF NOT EXISTS countries(name text, data text)")
-
+    
     for country in data_countries:
         country_name = json.dumps(country["name"])
         country_data = json.dumps(country)
@@ -44,4 +44,24 @@ async def get_regions(request: Request):
         connection.commit()
 
     result = cursor.execute("SELECT json_extract(data, '$.region') as region, group_concat(json_extract(name, '$.common')) AS countries FROM countries GROUP BY json_extract(data, '$.region')")
+    return  result.fetchall()
+
+@app.get("/languages")
+async def get_languages(request: Request):
+    requests_client = request.app.requests_client
+    response = await requests_client.get("https://restcountries.com/v3.1/all")
+    data_countries = response.json()
+
+    connection = sqlite3.connect("thisdot_example.db")
+    cursor = connection.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS countries(name text, data text)")
+
+    for country in data_countries:
+        country_name = json.dumps(country["name"])
+        country_data = json.dumps(country)
+        cursor.execute("INSERT INTO countries VALUES(?, ?)", (country_name, country_data))
+        connection.commit()
+
+    result = cursor.execute("SELECT json_extract(data, '$.languages') as languages, group_concat(json_extract(name, '$.common')) AS countries FROM countries GROUP BY json_extract(data, '$.languages')")
     return  result.fetchall()
