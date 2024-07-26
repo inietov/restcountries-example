371ec8c88de7f02965593439ddd79674984f3076
Ivan Nieto Vivanco <inietov@gmail.com>
2024-07-26 15:52:49 -0600

    Add pagination to response

--- app/api/routes/countries.py
+++ app/api/routes/countries.py
@@ -1,14 +1,22 @@
 from fastapi import APIRouter, HTTPException, Request
-from app.core.database import cursor, connection
+from app.core.database import cursor, connection, populate_database
 import json
 
 router = APIRouter()
 
 @router.get("/countries")
-async def get_countries(request: Request):
-    requests_client = request.app.requests_client
-    response = await requests_client.get("https://restcountries.com/v3.1/all")
-    return response.json()
+async def get_countries(size: int, page: int, request: Request):
+    table_count = cursor.execute("select count(name) from countries").fetchone()[0]
+
+    if table_count == 0:
+        await populate_database(request)
+
+    limit = size
+    offset = (page - 1) * limit
+
+    result = cursor.execute("SELECT * FROM countries LIMIT ? OFFSET ?", (limit, offset))
+
+    return result.fetchall()
 
 @router.get("/countries/{country_code}")
 async def get_countries_by_code(country_code: str, request: Request):
@@ -19,39 +27,20 @@ async def get_countries_by_code(country_code: str, request: Request):
 
 @router.get("/regions")
 async def get_regions(request: Request):
-    requests_client = request.app.requests_client
-    
     table_count = cursor.execute("select count(name) from countries").fetchone()[0]
 
-    print (table_count)
     if table_count == 0:
-        response = await requests_client.get("https://restcountries.com/v3.1/all")
-        data_countries = response.json()
-        
-        for country in data_countries:
-            country_name = json.dumps(country["name"])
-            country_data = json.dumps(country)
-            cursor.execute("INSERT INTO countries VALUES(?, ?)", (country_name, country_data))
-            connection.commit()
+        await populate_database(request)
 
     result = cursor.execute("SELECT json_extract(data, '$.region') as region, group_concat(json_extract(name, '$.common')) AS countries FROM countries GROUP BY json_extract(data, '$.region')")
     return  result.fetchall()
 
 @router.get("/languages")
 async def get_languages(request: Request):
-    requests_client = request.app.requests_client
-    
     table_count = cursor.execute("select count(name) from countries").fetchone()[0]
 
     if table_count == 0:
-        response = await requests_client.get("https://restcountries.com/v3.1/all")
-        data_countries = response.json()
-        
-        for country in data_countries:    
-            country_name = json.dumps(country["name"])
-            country_data = json.dumps(country)
-            cursor.execute("INSERT INTO countries VALUES(?, ?)", (country_name, country_data))
-            connection.commit()
+        await populate_database(request)
 
     result = cursor.execute("SELECT json_extract(data, '$.languages') as languages, group_concat(json_extract(name, '$.common')) AS countries FROM countries GROUP BY json_extract(data, '$.languages')")
     return  result.fetchall()
--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,20 @@
+from fastapi import Request
 import sqlite3
+import json
 
 connection = sqlite3.connect("thisdot_example.db")
 cursor = connection.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS countries(name text, data text)")
+
+async def populate_database(request: Request):
+    requests_client = request.app.requests_client
+
+    response = await requests_client.get("https://restcountries.com/v3.1/all")
+    data_countries = response.json()
+        
+    for country in data_countries:    
+        country_name = json.dumps(country["name"])
+        country_data = json.dumps(country)
+        cursor.execute("INSERT INTO countries VALUES(?, ?)", (country_name, country_data))
+        connection.commit()
