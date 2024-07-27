393532bc3ebd8f03b08e824f6df9ec1df81c0607
Ivan Nieto Vivanco <inietov@gmail.com>
2024-07-26 23:04:14 -0600

    Rollback

--- app/api/routes/dadjokes.py
+++ app/api/routes/dadjokes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Request, FastAPI
+from fastapi import APIRouter, HTTPException, Request
 
 router = APIRouter()
 
 @router.get("/")
-async def get_dad_joke(request: Request, app: FastAPI):
+async def get_dad_joke(request: Request):
     url = "https://official-joke-api.appspot.com/random_joke"
     requests_client = request.app.requests_client
     response = await requests_client.get(url)
