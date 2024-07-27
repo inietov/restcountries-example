bead1a85203407882b5cc047d5354322638ceda5
Ivan Nieto Vivanco <inietov@gmail.com>
2024-07-26 23:01:27 -0600

    Test fix

--- app/api/routes/dadjokes.py
+++ app/api/routes/dadjokes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, FastAPI
 
 router = APIRouter()
 
 @router.get("/")
-async def get_dad_joke(request: Request):
+async def get_dad_joke(request: Request, app: FastAPI):
     url = "https://official-joke-api.appspot.com/random_joke"
     requests_client = request.app.requests_client
     response = await requests_client.get(url)
